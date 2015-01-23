2dfb8cb553b8447339b9b626784251269d5fb554
islavov <ivelin.slavov@gmail.com>
2015-01-23 23:59:23 +0200

    random testing and stuff

--- Game.js
+++ Game.js
@@ -27,25 +27,66 @@ BasicGame.Game = function (game) {
 
 BasicGame.Game.prototype = {
 
+    cursors: null,
+
     preload: function(){
         this.load.image('tiles', 'tilemaps/tiles.png', 64, 64);
+        this.load.image('char', 'images/charmanderd.png', 64, 64);
         this.load.tilemap('level', 'tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
     },
 
     create: function () {
-        this.stage.backgroundColor = "#000000";
+        this.stage.backgroundColor = "#ffffff";
         this.map = this.add.tilemap("level");
         this.map.addTilesetImage("tiles");
-        layer = this.map.createLayer("Tile Layer 1");
-        layer.resizeWorld();
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        this.world.setBounds(0, 0, 1920, 1024);
+
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        noburnlayer = this.map.createLayer("nonburning");
+        burnlayer = this.map.createLayer("burning");
+
+
+        this.sprite = this.add.sprite(this.world.top, this.world.left, 'char');
+        this.sprite.anchor.set(0.5);
+        this.physics.arcade.enable(this.sprite);
+        this.sprite.body.setSize(32, 32, 16, 16);
+
+        //  We'll set a lower max angular velocity here to keep it from going totally nuts
+        this.sprite.body.maxAngular = 500;
+
+        //  Apply a drag otherwise the sprite will just spin and never slow down
+        this.sprite.body.angularDrag = 50;
+
+        this.camera.follow(this.sprite);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
     },
 
     update: function () {
 
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = 0;
+        this.sprite.body.angularVelocity = 0;
+
+        if (this.cursors.left.isDown)
+        {
+            this.sprite.body.angularVelocity = -300;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.sprite.body.angularVelocity = 300;
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.physics.arcade.velocityFromAngle(this.sprite.angle, 300, this.sprite.body.velocity);
+        }
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
 
+    },
+
+    render: function (){
+        this.debug;
     },
 
     quitGame: function (pointer) {
