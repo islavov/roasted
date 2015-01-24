7e7807bb09e5ed17fd5a21748177b56a7d7b4109
islavov <ivelin.slavov@gmail.com>
2015-01-24 23:20:55 +0200

    camera follow only up

--- objects/player.js
+++ objects/player.js
@@ -5,7 +5,7 @@ Player = function (game, speed, bounce, velocity, xCord, yCord) {
     this.speed = speed;
     this.velocity = velocity;
     game.physics.arcade.enable(this);
-    this.body.gravity.y = 300;
+    this.body.gravity.y = 730;
     this.body.bounce.set(0.1);
     this.body.tilePadding.set(32);
     this.body.collideWorldBounds = true;
@@ -16,6 +16,8 @@ Player = function (game, speed, bounce, velocity, xCord, yCord) {
     this.animations.play("burn", 24, true);
 
     game.camera.follow(this, Phaser.Camera.FOLLOW_PLATFORMER);
+    this.lastPositionY = this.position.y;
+
 };
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -60,6 +62,16 @@ Player.prototype.movePlayer = function (cursors, velocity, flag) {
     }
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && this.body.onFloor()) {
-        this.body.velocity.y = -300;
+        this.body.velocity.y = -940;
+
     }
+
+    if (this.lastPositionY > this.position.y){
+        this.game.camera.follow(this, Phaser.Camera.FOLLOW_PLATFORMER);
+        this.lastPositionY = this.position.y;
+    }
+    if (this.lastPositionY < this.position.y) {
+        this.game.camera.unfollow();
+    }
+
 }
