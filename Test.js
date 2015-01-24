8c9fa93117ca05f706bc53066939f3b313492ac8
pavkataepich <pavel.agov@gmail.com>
2015-01-24 20:22:45 +0200

    Animation and other stuff

--- Test.js
+++ Test.js
@@ -1,8 +1,10 @@
 var game = new Phaser.Game(640, 480, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 function preload() {
-
-	game.load.spritesheet('dude', 'assets/chick.png', 64, 64);
+	
+	
+	game.load.atlasJSONHash('dude', 'assets/chick1.png', 'assets/chick1.json');
 	game.load.image('strawberry', 'assets/strawberry.png');
+	game.load.image('icecube', 'assets/icecube.jpg');
 	game.load.tilemap('map', 'tilemaps/level2.json',null, Phaser.Tilemap.TILED_JSON);
 	game.load.image('bricktile', 'tilemaps/rock.jpg');
 	game.load.image('woodtile', 'tilemaps/tileswood.jpg');
@@ -15,14 +17,16 @@ var sprite;
 var cursors;
 var burning;
 var nonburning;
+var velocity=0;
+var flag = false;;
 
 
 var platforms;
 
 function create() {
-
-    player = new Player(game, 150, 0.1, -350, 0, 5000 );
-
+	
+    player = new Player(game, 150, 0.5, -350, 0, 5000 );
+	
 	createMap();
 	
     //  We're going to be using physics, so enable the Arcade Physics system
@@ -37,7 +41,7 @@ function create() {
 
 	//create the strawberry
 	strawberry = powerups.create(100 , 5000,'strawberry');
-	
+	icecube = powerups.create(400, 5000, 'icecube');
 	
 	game.physics.arcade.enable(this.burning);
 	game.physics.arcade.enable(this.nonburning);
@@ -46,6 +50,8 @@ function create() {
 
 function createMap()
 {
+	
+	
 	//CreateMap
 	game.stage.backgroundColor='#787878';
 	map = game.add.tilemap('map');
@@ -57,23 +63,24 @@ function createMap()
 	burning.resizeWorld();
 	nonburning.resizeWorld();
 	
-	
 	map.setCollisionBetween(1, 100000, true, 'burning');
 	map.setCollisionBetween(1, 100000, true, 'nonburning');
 }
 function update() {
-
+	
+	player.animations.play('burn',24,true);
      //  Collide the player and the stars with the platforms
       game.physics.arcade.collide(player, burning);
-	  game.camera.follow(player);
+	  //game.camera.follow(player);
 	  game.physics.arcade.collide(player, nonburning);
 	
 	 //collide with strawberry
 	
 	 game.physics.arcade.collide(player, strawberry, collisionHandler, null, this);
+	 game.physics.arcade.collide(player, icecube, collisionIceHandler, null, this);
 	
 	//Moving the player
-	 player.movePlayer(game.input.keyboard.createCursorKeys());
+	 player.movePlayer(game.input.keyboard.createCursorKeys(),velocity, flag);
 	
      
 }
@@ -87,14 +94,25 @@ function collisionHandler (obj1, obj2) {
 	timer = game.time.create(false);
 	game.time.events.add(Phaser.Timer.SECOND * 5, updateSpeed, this);
 }
+
+function collisionIceHandler (obj1, obj2) {
+	velocity = 200;
+	
+	flag = true;
+	timer = game.time.create(false);
+	game.time.events.add(Phaser.Timer.SECOND * 5, updateVelocity, this);
+}
 function updateSpeed(){
 
 player.setSpeed(150);
 
 }
 
+function updateVelocity(){
+	flag = false;
+}
 function render() {
 
-    game.debug.cameraInfo(game.camera, 32, 32);
+    //game.debug.cameraInfo(game.camera, 32, 32);
     //game.debug.spriteCoords(player, 32, 500);
 }
--- objects/player.js
+++ objects/player.js
@@ -2,86 +2,81 @@ Player = function (game, speed, bounce, velocity, xCord, yCord) {
 
     //  We call the Phaser.Sprite passing in the game reference
     //  We're giving it a random X/Y position here, just for the sake of this demo - you could also pass the x/y in the constructor
-    Phaser.Sprite.call(this, game, xCord, yCord, 'chick');
-
-    //this.anchor.setTo(0.5, 0.5);
+    Phaser.Sprite.call(this, game, xCord, yCord, 'dude');
 
     this.speed = speed;
-    this.bounce = bounce;
-    this.velocity = velocity;
-
-    this.jumping = false;
-
-    game.physics.arcade.enable(this);
-    game.camera.follow(this);
-
-    this.body.gravity.y = 400;
-    this.body.mass = 12;
-    this.body.collideWorldBounds = true;
-    this.body.debug = true;
-
-    //this.animations.add('left', [0, 1, 2, 3], 10, true);
-    //this.animations.add('right', [5, 6, 7, 8], 10, true);
-
+	this.velocity = velocity;
+	game.physics.arcade.enable(this);
+	this.body.gravity.y = 300;
+	this.body.bounce.set(0.5);
+	this.body.tilePadding.set(32);
+    this.body.collideWorldBounds = true
+	
+    this.animations.add('burn');
+	 
     game.add.existing(this);
-
+	game.camera.follow(this, Phaser.Camera.FOLLOW_PLATFORMER);
 };
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
-
-Player.prototype.getSpeed = function () {
-    return this.speed;
-}
-Player.prototype.setSpeed = function (addSpeed) {
-    this.speed = addSpeed;
-}
-
-Player.prototype.getVelocity = function () {
-    return this.velocity;
-};
-
-Player.prototype.Flip = function (orientation) {
-    this.anchor.setTo(.5, 1); //so it flips around its middle
-    this.scale.x = orientation; //facing default direction
-}
-
-Player.prototype.movePlayer = function (cursors) {
-
-    //reset player velocity
-    this.body.velocity.x = 0;
-    var speed = this.speed;
-
-    if (this.body.onFloor()){
-        this.jumping = false;
-    }
-
-
-    if (cursors.left.isDown) {
-        //  Move to the left
-        this.body.velocity.x -= speed;
-        this.Flip(1);
-        //this.animations.play('left');
-    }
-    else if (cursors.right.isDown) {
-        //  Move to the right
-        this.body.velocity.x += speed;
-        this.Flip(-1);
-
-        //this.animations.play('right');
-    }
-    else {
-        //  Stand still
-        this.animations.stop();
-
-        this.frame = 4;
-    }
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && this.body.onFloor()) {
-        this.body.velocity.y = -300;
-        this.jumping = true;
-    }
-
-}
-
+	
+	 Player.prototype.getSpeed = function () {
+	  return this.speed;
+	}
+	 Player.prototype.setSpeed = function (addSpeed) {
+	  this.speed = addSpeed;	
+	}
+	 	Player.prototype.setVelocity = function(velocity)
+	{
+		this.velocity.x = velocity;
+	}
+	 Player.prototype.getVelocity = function () {
+	 return this.velocity;
+	};
+	
+	Player.prototype.movePlayer = function (cursors,velocity, flag) {
+	
+	//reset player velocity
+	if(!flag)
+	{
+		this.body.velocity.x = 0;
+	}
+	
+	 if (cursors.left.isDown)
+	 {
+		//  Move to the left 	
+	 velocity *= -1;
+	 this.body.velocity.x = -this.speed;
+	 this.Flip(1);
+	 }
+	 else if (cursors.right.isDown)
+	 {
+	 //  Move to the right
+	 this.body.velocity.x = this.speed;
+	 this.Flip(-1);
+	 }
+	 else
+	 {
+	 //  Stand still
+	 this.animations.stop();
+	
+	 this.frame = 4;
+	 }
+	 //  Allow the player to jump if they are touching the ground.
+	 if (cursors.up.isDown && player.body.onFloor())
+	 {
+	 this.body.velocity.y = -300;
+	 }
+	 if(cursors.down.isDown)
+	 {
+	 this.body.velocity.y=350;
+	 }
+	 
+	 Player.prototype.Flip=function(orientation){
+	this.anchor.setTo(.5, 1); //so it flips around its middle
+	this.scale.x = orientation; //facing default direction
+	this.scale.x = orientation; //flipped
+ }
+	 }
