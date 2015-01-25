be126ad8433c1ff5b3f88956a5db00bcfafff4d2
pavkataepich <pavel.agov@gmail.com>
2015-01-25 14:21:07 +0200

    our changes

---
+++ ourchanges/blocks.js
@@ -0,0 +1,104 @@
+/**
+ * Created by islavov on 1/24/15.
+ */
+
+burningBlock = function(game, x, y, key, frame){
+    Phaser.Sprite.call(this, game, x, y, key, frame);
+    game.physics.arcade.enable(this);
+
+    this.initialKey = key;
+    this.isBurning = false;
+    this.resistent = false;
+    this.body.immovable = true;
+}
+
+burningBlock.prototype = Object.create(Phaser.Sprite.prototype);
+burningBlock.prototype.constructor = burningBlock;
+burningBlock.prototype.startBurn = function(){
+    if (!this.isBurning && !this.resistent){
+        this.isBurning = true;
+        this.game.time.events.add(Phaser.Timer.SECOND/6, this.kill, this);
+    }
+
+};
+
+
+powerUp = function(game, x, y, key, frame){
+    Phaser.Sprite.call(this, game, x, y, key, frame);
+    game.physics.arcade.enable(this);
+	
+	this.active = false;
+    this.body.immovable = true;
+	this.body.gravity.y = 300;
+	this.animations.add('anim');
+	this.animations.play('anim',10,true);
+}
+
+powerUp.prototype = Object.create(Phaser.Sprite.prototype);
+powerUp.prototype.constructor = powerUp;
+powerUp.prototype.apply = function(game, player){
+
+}
+powerUp.prototype.revert = function(game, player){
+
+}
+
+blockIce = function(game, x, y, key, frame){
+    powerUp.call(this, game, x, y, key, frame);
+};
+
+
+blockIce.prototype = Object.create(powerUp.prototype);
+blockIce.prototype.constructor = powerUp;
+blockIce.prototype.apply = function(game, player){
+    this.kill();
+		player.updateFlag();
+		player.updateVelocity(300);
+		
+		
+		for (var bidx in game.burning_blocks.children){
+			var child = game.burning_blocks.children[bidx];
+			child.loadTexture("f" + child.initialKey);
+			child.resistent = true;
+		}
+		this.game.time.events.add(Phaser.Timer.SECOND * 10, this.revert, this, game, player);
+	
+}
+blockIce.prototype.revert = function(game, player){
+    for (var bidx in game.burning_blocks.children){
+        var child = game.burning_blocks.children[bidx];
+        child.loadTexture(child.initialKey);
+        child.resistent = false;
+    }
+	player.updateFlag();
+}
+
+gasTube = function(game, x, y, key, frame){
+	powerUp.call(this, game, x, y, key, frame);
+};
+
+gasTube.prototype = Object.create(powerUp.prototype);
+gasTube.prototype.constructor = powerUp;
+gasTube.prototype.apply = function(game, player) {
+	this.kill();
+		player.updateSpeed(850);
+		
+		this.game.time.events.add(Phaser.Timer.SECOND * 10, this.revert, this, game, player);
+	}
+
+gasTube.prototype.revert = function(game, player) {
+	player.updateSpeed(550);
+}
+
+
+waterCup = function(game, x, y, key, frame){
+	powerUp.call(this, game, x, y, key, frame);
+};
+
+waterCup.prototype = Object.create(powerUp.prototype);
+waterCup.prototype.constructor = powerUp;
+
+waterCup.prototype.apply = function(game, player) {
+	this.kill();
+	player.updateLifespan(7000);
+}
---
+++ ourchanges/player.js
@@ -0,0 +1,122 @@
+Player = function (game, speed, bounce, velocity, xCord, yCord) {
+
+
+
+    Phaser.Sprite.call(this, game, xCord, yCord, 'chick1');
+
+    this.speed = speed;
+    this.velocity = velocity;
+    game.physics.arcade.enable(this);
+	this.flag = false;
+    this.lifespan = 30000;
+
+    this.body.gravity.y = 730;
+    this.body.bounce.set(0.1);
+    this.body.tilePadding.set(32);
+    this.anchor.setTo(.5, 1); //so it flips around its middle
+    this.scale.x = -1;
+    this.body.collideWorldBounds = true;
+
+    game.add.existing(this);
+
+    this.animations.add("burn");
+    this.animations.play("burn", 24, true);
+
+    game.camera.follow(this, Phaser.Camera.FOLLOW_PLATFORMER);
+    this.lastPositionY = this.position.y;
+
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.kill = function(){
+    this.loadTexture('chick4');
+    this.animations.add('dead');
+    this.animations.play('dead', 16, true);
+    this.lifespan = 0;
+	microwave.play();
+//    this.game.state.start('Game');
+
+}
+
+Player.prototype.updateSpeed = function(newSpeed) {
+	this.speed = newSpeed;
+	}
+Player.prototype.updateLifespan = function(addLifespan) {
+	this.lifespan += addLifespan;
+		if(this.lifespan >= 30000) this.lifespan = 30000;
+	}
+Player.prototype.updateVelocity = function(newVelocity){
+	this.velocity = newVelocity;
+}
+Player.prototype.updateFlag = function(){
+	this.flag = !this.flag;
+}	
+Player.prototype.updatePlayer = function(cursors){
+    this.updateState();
+    if (this.lifespan <= 0){
+        this.body.velocity.x = 0;
+        return
+    }
+
+    this.movePlayer(cursors);
+}
+
+Player.prototype.updateState = function() {
+	if (this.lifespan > 10000 && this.lifespan < 20000 && this.key != 'chick2')
+	{
+		this.loadTexture('chick2');
+		this.animations.add('overburn');
+		this.animations.play('overburn', 30, true);
+	}
+	else if ( this.lifespan > 0 && this.lifespan < 10000  && this.key != 'chick3' )
+	{
+		this.loadTexture('chick3');
+		this.animations.add('roasted');
+		this.animations.play('roasted', 60, true);
+	}
+}
+Player.prototype.Flip = function (orientation) {
+    this.scale.x = orientation; //facing default direction
+}
+
+Player.prototype.movePlayer = function (cursors) {
+
+    //reset player velocity
+    if (!this.flag) {
+        this.body.velocity.x = 0;
+    }
+
+    if (cursors.left.isDown) {
+        //  Move to the left
+		chirpLeft.play();
+		this.velocity *= -1;
+        this.body.velocity.x = -this.speed;
+        this.Flip(1);
+		
+    }
+    else if (cursors.right.isDown) {
+        //  Move to the right
+		chirpRight.play();
+        this.body.velocity.x = this.speed;
+        this.Flip(-1);
+    }
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && this.body.onFloor()) {
+        this.body.velocity.y = -450;
+		chirpUp.play();
+
+    }
+    if (this.lastPositionY > this.position.y){
+        this.game.camera.follow(this, Phaser.Camera.FOLLOW_PLATFORMER);
+        this.lastPositionY = this.position.y;
+    }
+    if (this.lastPositionY < this.position.y) {
+        this.game.camera.unfollow();
+    }
+    if (this.game.camera.y && this.body.position.y > this.game.camera.view.y + this.game.camera.view.height){
+        this.kill();
+		microwave.play();
+    }
+}
